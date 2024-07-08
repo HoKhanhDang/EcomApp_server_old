df218320c804cea0b523a5fba1a32d55aeab53ac
HoKhanhDang <121717409+HoKhanhDang@users.noreply.github.com>
2024-07-08 11:45:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cors(
     {
-        origin: 'http://localhost:3000/',
+        origin: 'https://ecom-app-client.vercel.app/',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true
     }
